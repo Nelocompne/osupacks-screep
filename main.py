462c6aa1d2e28e453160386e7a3743533031aefc
Minn <qaqmango@outlook.com>
2023-04-01 14:58:47 +0800

    update yaml config 通过PyYAML实现读取yaml配置文件，代替过去麻烦的cli参数

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from requests_html import HTMLSession
 from tqdm import trange
-import json,time,random,argparse,os
+import yaml,json,time,random,os
 
-parser = argparse.ArgumentParser(description='help')
-parser.add_argument('-y', dest='types', type=str, help='曲包类型. e.g. S/SM/ST/SC')
-parser.add_argument('-f', dest='frst', type=int, help='开始编号')
-parser.add_argument('-e', dest='end', type=int, help='截止编号')
-parser.add_argument('-o', dest='out', type=str, help='输出文件名(.json)')
-parser.add_argument('-t', dest='time', type=int, help='最长延迟时间(秒/s). 防止触发反爬')
-parser.add_argument('-c', dest='cookies', type=str, help='osu登录cookies文件')
-parser.add_argument('-p', dest='output', type=str, help='下载目录')
-args = parser.parse_args()
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+db_type = config['database']['type']
+db_star = config['database']['star']
+db_andy = config['database']['andy']
+db_time = config['database']['time']
+db_cookie = config['database']['cookie']
+db_dlpath = config['database']['dlpath']
+db_outjson = config['database']['outjson']
 
 session = HTMLSession()
 
-with open(args.cookies, 'r') as f:
+with open(db_cookie, 'r') as f:
     cookies = dict(cookies_are = f.read())
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0', 'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6'}
 
 link = dict()
 
-for i in trange(args.frst, args.end+1):
+for i in trange(db_star, db_andy+1):
     try:
-        time.sleep(random.uniform(1,args.time))
-        r = session.get(f"https://osu.ppy.sh/beatmaps/packs/{args.types}{i}", cookies=cookies, headers=headers)
+        time.sleep(random.uniform(1,db_time))
+        r = session.get(f"https://osu.ppy.sh/beatmaps/packs/{db_type}{i}", cookies=cookies, headers=headers)
         link_raw = r.html.find('div.beatmap-pack-description', first=True).find('a') # 下载链接
         for a in link_raw:
             links = a.absolute_links
@@ -46,8 +47,8 @@ for i in trange(args.frst, args.end+1):
             "name": t
         }
         link[i] = data
-    #os.system(f"mega-get {list(links)[0]} {args.output}") # on Windows
-    #os.system(f"mega-exec get {list(links)[0]} {args.output}") # on Linux
+    os.system(f"mega-get {list(links)[0]} {db_dlpath}") # on Windows
+    os.system(f"mega-exec get {list(links)[0]} {db_dlpath}") # on Linux
 
-with open(args.out, "w") as f:
+with open(db_outjson, "w") as f:
     json.dump(link, f, indent=4)
